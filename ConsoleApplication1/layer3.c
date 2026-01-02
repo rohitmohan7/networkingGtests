04ea1d4d76914c4540a09aedf8062a949a596287
rohit <rohitmohan7@gmail.com>
2026-01-02 16:44:19 -0600

    update

--- ConsoleApplication1/layer3.c
+++ ConsoleApplication1/layer3.c
@@ -16,6 +16,8 @@ void l3Init() {
 }
 
 bool getl3Pkt(struct L2Pkt* l2pkt, bool* xferMst, uint8_t* addr, uint8_t port) {
+	bool checkPrioPending = false;
+	*xferMst = true;
 	for (uint8_t prio = 0; prio < MAX_PRIORITY; prio++) {
 		for (int pos = 0; pos < MAX_POS; pos++) {
 			stream_t* s = &streams[pos];
@@ -23,12 +25,35 @@ bool getl3Pkt(struct L2Pkt* l2pkt, bool* xferMst, uint8_t* addr, uint8_t port) {
 
 			if (((s->gateway & 0xFF00) == (port_addr[port] & 0xFF00)) 
 				&& ps->head_page != INVALID_PAGE) {
-				// set header
-				L3Hdr* l3hdr = &l2pkt->msg.pdu.l3pkt.hdr;
 
+				if (checkPrioPending) {
+					*xferMst = false;
+					return true;
+				}
+
+				// set header
+				L3Pkt* l3Pkt = &l2pkt->msg.pdu.l3pkt;
+				L3Hdr* l3Hdr = &l3Pkt->hdr;
+			
+				l3Hdr->src = port_addr[port];
+				l3Hdr->dst = s->dst;
+				//l3hdr->ttl = TODO
+				l3Hdr->prio = prio;
+ 
 				uint8_t len = L3_FRAME_SIZE;
+				l3Pkt->head_page = ps->head_page;
+				l3Pkt->head_off = ps->head_off;
+
+				uint8_t currHd = l3Pkt->head_page;
+
+				while (len > 0) {
+					if (l3Pkt->head_page == ps->tail_page) {
+
+					}
+				}
+				checkPrioPending = true;
 			}
 		}
 	}
-	return false;
+	return checkPrioPending;
 }
